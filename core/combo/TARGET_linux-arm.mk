3d6293af209b72b030796fbc813c4446f1870806
Mathieu J. Poirier <mathieu.poirier@linaro.org>
2012-03-16 13:56:42 -0600

    snowball: disabling -03 optimzation until we have new mali libs
    The '-03' strips off '__aeabi_llsr' from libgcc.a, something that
    prevents the mali libs from loading.  Those libs don't include the
    symbol and expect an external entity to provide it.
    
    This is only a temporary fix allowing snowball users to get a
    working UI environment.  This should be reverted when we get new
    mali libs, something we are currently working on.
    
    Change-Id: Ifa4cc0b1671caa7a2cf5632b60b2aa5417b1a16c
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -66,20 +66,34 @@ endif
 
 TARGET_NO_UNDEFINED_LDFLAGS := -Wl,--no-undefined
 
+ifneq ($(TARGET_PRODUCT),snowball)
 TARGET_arm_CFLAGS :=    -O3 \
                         -fomit-frame-pointer \
                         -fstrict-aliasing    \
                         -funswitch-loops
+else
+TARGET_arm_CFLAGS :=    -O2 \
+                        -fomit-frame-pointer \
+                        -fstrict-aliasing    \
+                        -funswitch-loops
+endif
 
 # Modules can choose to compile some source as thumb. As
 # non-thumb enabled targets are supported, this is treated
 # as a 'hint'. If thumb is not enabled, these files are just
 # compiled as ARM.
 ifeq ($(ARCH_ARM_HAVE_THUMB_SUPPORT),true)
+ifneq ($(TARGET_PRODUCT),snowball)
 TARGET_thumb_CFLAGS :=  -mthumb \
                         -O3 \
                         -fomit-frame-pointer \
                         -fno-strict-aliasing
+else
+TARGET_thumb_CFLAGS :=  -mthumb \
+                        -Os \
+                        -fomit-frame-pointer \
+                        -fno-strict-aliasing
+endif
 else
 TARGET_thumb_CFLAGS := $(TARGET_arm_CFLAGS)
 endif
